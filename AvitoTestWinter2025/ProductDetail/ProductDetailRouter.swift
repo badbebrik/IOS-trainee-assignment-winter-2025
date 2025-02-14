9786c979070230332fcdaeb89caf730b138d284f
Viktoria Serikova <viktoria.serikova2016@yandex.ru>
2025-02-14 04:08:37 +0300

    Add ProductDetailInteractor, ProductDetailRouter

---
+++ AvitoTestWinter2025/ProductDetail/ProductDetailRouter.swift
@@ -0,0 +1,12 @@
+//
+//  ProductDetailRouter.swift
+//  AvitoTestWinter2025
+//
+//  Created by Виктория Серикова on 14.02.2025.
+//
+
+import UIKit
+
+final class ProductDetailRouter: ProductDetailRouterProtocol {
+    weak var viewController: UIViewController?
+}
