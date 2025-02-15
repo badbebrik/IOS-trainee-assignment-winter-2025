46d67ddcc8e9f07a50ad66146d39e682af2409c9
Viktoria Serikova <viktoria.serikova2016@yandex.ru>
2025-02-16 01:32:46 +0300

    Add CartStorage

---
+++ AvitoTestWinter2025/Storage/CartStorage.swift
@@ -0,0 +1,32 @@
+//
+//  CartStorage.swift
+//  AvitoTestWinter2025
+//
+//  Created by Виктория Серикова on 16.02.2025.
+//
+
+import Foundation
+
+protocol CartStorageProtocol {
+    func save(cartItems: [CartItem])
+    func load() -> [CartItem]
+}
+
+final class CartStorage: CartStorageProtocol {
+    private let key = "CartItems"
+
+    func save(cartItems: [CartItem]) {
+        if let data = try? JSONEncoder().encode(cartItems) {
+            UserDefaults.standard.set(data, forKey: key)
+        }
+    }
+
+    func load() -> [CartItem] {
+        guard let data = UserDefaults.standard.data(forKey: key), let items = try? JSONDecoder().decode([CartItem].self, from: data) else {
+            return []
+        }
+
+        return items
+
+    }
+}
