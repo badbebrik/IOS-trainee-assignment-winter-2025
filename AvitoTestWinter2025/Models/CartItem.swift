3e75657c7d963a4634a7bbf30ecba587de9adf19
Viktoria Serikova <viktoria.serikova2016@yandex.ru>
2025-02-16 01:28:20 +0300

    Create CartItem Model

---
+++ AvitoTestWinter2025/Models/CartItem.swift
@@ -0,0 +1,17 @@
+//
+//  CartItem.swift
+//  AvitoTestWinter2025
+//
+//  Created by Виктория Серикова on 16.02.2025.
+//
+
+import Foundation
+
+struct CartItem: Codable, Equatable {
+    let product: Product
+    var quantity: Int
+
+    static func == (lhs: CartItem, rhs: CartItem) -> Bool {
+        lhs.product.id == rhs.product.id
+    }
+}
