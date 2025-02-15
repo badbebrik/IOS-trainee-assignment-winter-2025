c3230b636b7caa985a310ee13b0d9c759e92bf17
Viktoria Serikova <viktoria.serikova2016@yandex.ru>
2025-02-16 02:04:53 +0300

    Add protocols for Cart module

---
+++ AvitoTestWinter2025/Cart/CartProtocols.swift
@@ -0,0 +1,36 @@
+//
+//  CartProtocols.swift
+//  AvitoTestWinter2025
+//
+//  Created by Виктория Серикова on 16.02.2025.
+//
+
+// MARK: View
+protocol CartViewProtocol: AnyObject {
+    func showCartItems(_ items: [CartItem])
+}
+
+// MARK: Presenter
+protocol CartPresenterProtocol: AnyObject {
+    func viewDidLoad()
+    func didTapShare()
+    func didUpdateCart()
+    func didSelectCartItem(_ item: CartItem)
+    func didRemoveCartItem(_ item: CartItem)
+    func didClearCart()
+    func moveCartItem(from sourceIndex: Int, to destinationIndex: Int)
+}
+
+// MARK: Interactor
+protocol CartInteractorProtocol: AnyObject {
+    func fetchCartItems()
+    func removeItem(_ item: CartItem)
+    func clearCart()
+    func moveItem(from sourceIndex: Int, to destinationIndex: Int)
+}
+
+// MARK: Router
+protocol CartRouterProtocol: AnyObject {
+    func navigateToProductDetail(with product: Product)
+    func shareCart(with text: String)
+}
